cb25c7dfc80ef26aa8b6eb707d4097e07f70edfc
felipe.both <felipe.sfreitas14@gmail.com>
2024-01-12 15:02:26 -0300

    Insert the solution to day 09 problem

--- 100-dias-java/src/day09.java
+++ 100-dias-java/src/day09.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class day09 {
+public class day09 {
+    public int trap(int[] height){
+        if(height == null || height.length == 0){
+            return 0;
+        }
+        int left = 0, right = height.length -1;
+        int leftMax = 0, rightMax = 0;
+        int result = 0;
+        while (left < right){
+            if (height[left] <= height[right]){
+                if(height[left] >= height[right]){
+                    leftMax  = height[left];
+                }else {
+                    result += leftMax - height[left];
+                }
+                right --;
+            }
+        }
+        return result;
+    }
 }
